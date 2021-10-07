be3bd31a0d47230d68b4be534209db36f42e05ad
eugmani <eugmani@gmail.com>
2021-10-07 14:31:21 +0200

    añado clase test RolController

---
+++ Usuarios/src/test/java/com/microservicios/usuario/controller/RolControllerTest.java
@@ -0,0 +1,26 @@
+package com.microservicios.usuario.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(controllers = RolController.class)
+class RolControllerTest {
+
+	@Test
+	void testFindByRolPageable() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCrearRolBindingResult() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEditarRolBindingResult() {
+		fail("Not yet implemented");
+	}
+
+}
